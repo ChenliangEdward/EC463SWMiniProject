dd7041aa84befbbd6973ce75b87eefa4a73b9fbc
Elizabeth Diamond <diamond7@bu.edu>
2021-09-15 08:50:27 -0400

    Updated project + node_modules
     Finished the login screen to include email/password text fields

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native';
-import { SocialIcon } from 'react-native-elements'
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin'; 
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}> 
         Recipe Builder
       </Text>
-      <Button title="Login"></Button>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -26,6 +50,31 @@ const styles = StyleSheet.create({
   },
   title: {
     fontFamily: 'Futura-CondensedMedium',
-    fontSize: 75,
+    fontSize: 70,
+    marginBottom: 40,
+  },
+  inputView: {
+    backgroundColor: "#87cefa",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  loginBtn:
+  {  
+    width:"80%",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    backgroundColor:"#20b2aa",
   }
 });
