6e2781e71992cc248ba80039179e6addc20f7860
Edward <edward@linux.linux.com>
2021-09-18 02:07:54 -0400

    add recipe works now

--- screens/Recipes.js
+++ screens/Recipes.js
@@ -5,34 +5,33 @@ import { useState } from "react";
 
 function Recipes({ navigation }) {
   const [recipe_name, setRecipeName] = useState("");
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.recipeBtn}>
-            <Button
-            title="Add Recipe"
-            onPress={() => 
-              {return (
-                <View style={styles.container}>
-                  <TouchableHighlight onPress={() => navigation.navigate("Details")}>
-                    <View style={styles.recipeBox}>
-                      <TextInput
-                        style={styles.TextInput}
-                        placeholder="add recipe name"
-                        placeholderTextColor="#003f5c"
-                        onChangeText={(recipe_name) => setRecipeName(recipe_name)}
-                      />
-                    </View>
-                  </TouchableHighlight>
-                </View>  
-              )}}
-            />
-            <Button
-            title="Add Ingredient"
-            onPress={() => navigation.navigate("FoodSearch")}
-            />
+  const [showResults, setShowResults] = useState(false);
+  const onClick = () => setShowResults(true);
+  const RecipeThing = () => (
+    <View style={styles.container}>
+      <TouchableHighlight onPress={() => navigation.navigate("Details")}>
+        <View style={styles.recipeBox}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="add recipe name"
+            placeholderTextColor="#003f5c"
+            onChangeText={(recipe_name) => setRecipeName(recipe_name)}
+          />
         </View>
-      </View>  
-    );
-  }
-  export default Recipes;
+      </TouchableHighlight>
+    </View>
+  );
+  return (
+    <View style={styles.container}>
+      <View style={styles.recipeBtn}>
+        <Button title="Add Recipe" onPress={onClick} />
+        {showResults ? <RecipeThing /> : null}
+        <Button
+          title="Add Ingredient"
+          onPress={() => navigation.navigate("FoodSearch")}
+        />
+      </View>
+    </View>
+  );
+}
+export default Recipes;
